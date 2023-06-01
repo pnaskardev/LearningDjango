8cdbcb4486e9992934de246b8bf6b2168f92693e
pnaskardev <priyanshunaskar89@gmail.com>
2023-06-01 21:49:47 +0530

    Post model added

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-posts=[
-    {
-        'author':'CoreyMS',
-        'title':'Blog Post 1',
-        'content':'First Post content',
-        'date_posted':"June 1 2023"
-    },
-    {
-        'author':'Priyanshu Naskar',
-        'title':'Blog Post 2',
-        'content':'Second Post content',
-        'date_posted':"June 2 2023"
-    }
-]
+from .models import Post
 
 def home(request):
     context={
-        'posts':posts,
+        'posts':Post.objects.all(),
     }
     return render(request, 'blog/home.html',context)
 
